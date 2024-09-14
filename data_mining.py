1407cebf1157a2a0589503611b9b5026f2fb7622
anawinter53 <anatolia.winter@yahoo.com>
2024-09-14 22:37:56 +0100

    adding basic structure of tasks to work on

--- data_mining.py
+++ data_mining.py
@@ -1,3 +1,18 @@
 import csv
 
+# challenges with each dataset:
+# online_retail - 541k rows, time formats
+# spam - lots of different symbols, unstructured, includes links
+# students_performance - least challenging w/ 1k rows, most rows are set options, rows w/ scores have set range 0-100
 
+# online_retail - should manage large dataset & handle time format
+with open('online_retail.csv', newline='') as csvfile:
+    # retail = csv.reader(csvfile, delimiter=' ', quotechar= '|' )
+    retail = csv.DictReader(csvfile)
+    for row in retail:
+        print(row['Description'], row ['Quantity'])
+    
+# spam - should handle various inputs with links and symbols
+
+
+# students_performance - handle size of dataset and transform input into organised table, convert values?
